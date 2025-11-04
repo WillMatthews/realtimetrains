7d8458cc1c8e11298032a65f2559ae4e96c005c3
WillMatthews <will.a.matthews@me.com>
2025-11-04 20:32:56 +0000

    wrap time

---
+++ rtt/timetypes_test.go
@@ -0,0 +1,20 @@
+package rtt
+
+import (
+    "encoding/json"
+    "testing"
+)
+
+func TestTimeHHMM_Unmarshal(t *testing.T) {
+    var v struct{ T *TimeHHMM `json:"t"` }
+    if err := json.Unmarshal([]byte(`{"t":"0935"}`), &v); err != nil { t.Fatal(err) }
+    if v.T == nil || v.T.Hour()!=9 || v.T.Minute()!=35 { t.Fatalf("unexpected %#v", v.T) }
+    if v.T.String() != "0935" { t.Fatalf("string mismatch %s", v.T.String()) }
+}
+
+func TestTimeHHMMSS_Unmarshal(t *testing.T) {
+    var v struct{ T *TimeHHMMSS `json:"t"` }
+    if err := json.Unmarshal([]byte(`{"t":"235959"}`), &v); err != nil { t.Fatal(err) }
+    if v.T == nil || v.T.Hour()!=23 || v.T.Minute()!=59 || v.T.Second()!=59 { t.Fatalf("unexpected %#v", v.T) }
+    if v.T.String() != "235959" { t.Fatalf("string mismatch %s", v.T.String()) }
+}
--- rtt/types.go
+++ rtt/types.go
@@ -1,57 +1,56 @@
 package rtt
 
 // NOTE: These structs model only the documented fields for the simple pull API.
-// Many fields are left as basic types (string/bool/int) for now; helpers can be
-// added later for richer representations.
+// Time-related string fields are parsed into *TimeHHMM or *TimeHHMMSS wrappers.
 
 // Pair represents an origin/destination pair object.
 type Pair struct {
-    TIPLOC       string `json:"tiploc"`
-    Description  string `json:"description"`
-    WorkingTime  string `json:"workingTime"`
-    PublicTime   string `json:"publicTime"`
+    TIPLOC       string      `json:"tiploc"`
+    Description  string      `json:"description"`
+    WorkingTime  *TimeHHMMSS `json:"workingTime"`
+    PublicTime   *TimeHHMM   `json:"publicTime"`
 }
 
 // Location represents a calling (or pass) point in a service or search result.
 type Location struct {
-    RealtimeActivated             bool    `json:"realtimeActivated"`
-    TIPLOC                        string  `json:"tiploc"`
-    CRS                           *string `json:"crs"`
-    Description                   string  `json:"description"`
-    WTTBookedArrival              *string `json:"wttBookedArrival"`
-    WTTBookedDeparture            *string `json:"wttBookedDeparture"`
-    WTTBookedPass                 *string `json:"wttBookedPass"`
-    GBTTBookedArrival             *string `json:"gbttBookedArrival"`
-    GBTTBookedDeparture           *string `json:"gbttBookedDeparture"`
-    Origin                        []Pair  `json:"origin"`
-    Destination                   []Pair  `json:"destination"`
-    IsCall                        bool    `json:"isCall"`
-    IsCallPublic                  bool    `json:"isCallPublic"`
-    RealtimeArrival               *string `json:"realtimeArrival"`
-    RealtimeArrivalActual         *bool   `json:"realtimeArrivalActual"`
-    RealtimeArrivalNoReport       *bool   `json:"realtimeArrivalNoReport"`
-    RealtimeWTTArrivalLateness    *int    `json:"realtimeWttArrivalLateness"`
-    RealtimeGBTTAarrivalLateness  *int    `json:"realtimeGbttArrivalLateness"`
-    RealtimeDeparture             *string `json:"realtimeDeparture"`
-    RealtimeDepartureActual       *bool   `json:"realtimeDepartureActual"`
-    RealtimeDepartureNoReport     *bool   `json:"realtimeDepartureNoReport"`
-    RealtimeWTTDepartureLateness  *int    `json:"realtimeWttDepartureLateness"`
-    RealtimeGBTTDepartureLateness *int    `json:"realtimeGbttDepartureLateness"`
-    RealtimePass                  *string `json:"realtimePass"`
-    RealtimePassActual            *bool   `json:"realtimePassActual"`
-    RealtimePassNoReport          *bool   `json:"realtimePassNoReport"`
-    Platform                      *string `json:"platform"`
-    PlatformConfirmed             *bool   `json:"platformConfirmed"`
-    PlatformChanged               *bool   `json:"platformChanged"`
-    Line                          *string `json:"line"`
-    LineConfirmed                 *bool   `json:"lineConfirmed"`
-    Path                          *string `json:"path"`
-    PathConfirmed                 *bool   `json:"pathConfirmed"`
-    CancelReasonCode              *string `json:"cancelReasonCode"`
-    CancelReasonShortText         *string `json:"cancelReasonShortText"`
-    CancelReasonLongText          *string `json:"cancelReasonLongText"`
-    DisplayAs                     *string `json:"displayAs"`
-    ServiceLocation               *string `json:"serviceLocation"`
+    RealtimeActivated             bool        `json:"realtimeActivated"`
+    TIPLOC                        string      `json:"tiploc"`
+    CRS                           *string     `json:"crs"`
+    Description                   string      `json:"description"`
+    WTTBookedArrival              *TimeHHMMSS `json:"wttBookedArrival"`
+    WTTBookedDeparture            *TimeHHMMSS `json:"wttBookedDeparture"`
+    WTTBookedPass                 *TimeHHMMSS `json:"wttBookedPass"`
+    GBTTBookedArrival             *TimeHHMM   `json:"gbttBookedArrival"`
+    GBTTBookedDeparture           *TimeHHMM   `json:"gbttBookedDeparture"`
+    Origin                        []Pair      `json:"origin"`
+    Destination                   []Pair      `json:"destination"`
+    IsCall                        bool        `json:"isCall"`
+    IsCallPublic                  bool        `json:"isCallPublic"`
+    RealtimeArrival               *TimeHHMM   `json:"realtimeArrival"`
+    RealtimeArrivalActual         *bool       `json:"realtimeArrivalActual"`
+    RealtimeArrivalNoReport       *bool       `json:"realtimeArrivalNoReport"`
+    RealtimeWTTArrivalLateness    *int        `json:"realtimeWttArrivalLateness"`
+    RealtimeGBTTAarrivalLateness  *int        `json:"realtimeGbttArrivalLateness"`
+    RealtimeDeparture             *TimeHHMM   `json:"realtimeDeparture"`
+    RealtimeDepartureActual       *bool       `json:"realtimeDepartureActual"`
+    RealtimeDepartureNoReport     *bool       `json:"realtimeDepartureNoReport"`
+    RealtimeWTTDepartureLateness  *int        `json:"realtimeWttDepartureLateness"`
+    RealtimeGBTTDepartureLateness *int        `json:"realtimeGbttDepartureLateness"`
+    RealtimePass                  *TimeHHMM   `json:"realtimePass"`
+    RealtimePassActual            *bool       `json:"realtimePassActual"`
+    RealtimePassNoReport          *bool       `json:"realtimePassNoReport"`
+    Platform                      *string     `json:"platform"`
+    PlatformConfirmed             *bool       `json:"platformConfirmed"`
+    PlatformChanged               *bool       `json:"platformChanged"`
+    Line                          *string     `json:"line"`
+    LineConfirmed                 *bool       `json:"lineConfirmed"`
+    Path                          *string     `json:"path"`
+    PathConfirmed                 *bool       `json:"pathConfirmed"`
+    CancelReasonCode              *string     `json:"cancelReasonCode"`
+    CancelReasonShortText         *string     `json:"cancelReasonShortText"`
+    CancelReasonLongText          *string     `json:"cancelReasonLongText"`
+    DisplayAs                     *string     `json:"displayAs"`
+    ServiceLocation               *string     `json:"serviceLocation"`
 }
 
 // Service returned by /service endpoint.
